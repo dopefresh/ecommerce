f74fa408493a15d1621205b90383a80138491b98
dopefresh <dopefresh4000@gmail.com>
2021-04-18 10:08:25 +0400

    smaller content

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -142,9 +142,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # CRISPY FORMS 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
+"""
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 django_heroku.settings(locals())
-
+"""
